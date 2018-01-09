6a03696bec41b373bc1ef23d5a5ddf3f0699980d
“Benjamin <benfrank_v1340dev@outlook.com>
2018-01-09 13:10:09 -0600

    Mi primer commit :)

---
+++ HolaMundo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HolaMundo
+//
+//  Created by Benjamín on 09/01/18.
+//  Copyright © 2018 Benjamín. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hola Mundo :)");
+    }
+    return 0;
+}
